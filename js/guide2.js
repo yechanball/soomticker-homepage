381eb1ebf4036094a92437aba0402030610ad0b7
yechan-nfc <77843815+yechan-nfc@users.noreply.github.com>
2021-07-24 00:31:56 +0900

    Add FAQ tab menu

--- js/guide2.js
+++ js/guide2.js
@@ -18,3 +18,76 @@
         j.preventDefault();
     });
 })(jQuery);
+
+function selectSubMenu(num){
+    switch(num) {
+        case 1:
+            ($('.content-sub-menu-01')).addClass('selected-sub-menu');
+            document.all.subcontent01.style.visibility="visible";
+            ($('.content-sub-menu-02')).removeClass('selected-sub-menu');
+            document.all.subcontent02.style.visibility="hidden";
+            ($('.content-sub-menu-03')).removeClass('selected-sub-menu');
+            document.all.subcontent03.style.visibility="hidden";
+            ($('.content-sub-menu-04')).removeClass('selected-sub-menu');
+            document.all.subcontent04.style.visibility="hidden";
+            ($('.content-sub-menu-05')).removeClass('selected-sub-menu');
+            document.all.subcontent05.style.visibility="hidden";
+            break;
+        
+        case 2:
+            ($('.content-sub-menu-01')).removeClass('selected-sub-menu');
+            document.all.subcontent01.style.visibility="hidden";
+            ($('.content-sub-menu-02')).addClass('selected-sub-menu');
+            document.all.subcontent02.style.visibility="visible";
+            ($('.content-sub-menu-03')).removeClass('selected-sub-menu');
+            document.all.subcontent03.style.visibility="hidden";
+            ($('.content-sub-menu-04')).removeClass('selected-sub-menu');
+            document.all.subcontent04.style.visibility="hidden";
+            ($('.content-sub-menu-05')).removeClass('selected-sub-menu');
+            document.all.subcontent05.style.visibility="hidden";
+            break;
+
+        case 3:
+            ($('.content-sub-menu-01')).removeClass('selected-sub-menu');
+            document.all.subcontent01.style.visibility="hidden";
+            ($('.content-sub-menu-02')).removeClass('selected-sub-menu');
+            document.all.subcontent02.style.visibility="hidden";
+            ($('.content-sub-menu-03')).addClass('selected-sub-menu');
+            document.all.subcontent03.style.visibility="visible";
+            ($('.content-sub-menu-04')).removeClass('selected-sub-menu');
+            document.all.subcontent04.style.visibility="hidden";
+            ($('.content-sub-menu-05')).removeClass('selected-sub-menu');
+            document.all.subcontent05.style.visibility="hidden";
+            break;
+
+        case 4:
+            ($('.content-sub-menu-01')).removeClass('selected-sub-menu');
+            document.all.subcontent01.style.visibility="hidden";
+            ($('.content-sub-menu-02')).removeClass('selected-sub-menu');
+            document.all.subcontent02.style.visibility="hidden";
+            ($('.content-sub-menu-03')).removeClass('selected-sub-menu');
+            document.all.subcontent03.style.visibility="hidden";
+            ($('.content-sub-menu-04')).addClass('selected-sub-menu');
+            document.all.subcontent04.style.visibility="visible";
+            ($('.content-sub-menu-05')).removeClass('selected-sub-menu');
+            document.all.subcontent05.style.visibility="hidden";
+            break;
+
+        case 5:
+            ($('.content-sub-menu-01')).removeClass('selected-sub-menu');
+            document.all.subcontent01.style.visibility="hidden";
+            ($('.content-sub-menu-02')).removeClass('selected-sub-menu');
+            document.all.subcontent02.style.visibility="hidden";
+            ($('.content-sub-menu-03')).removeClass('selected-sub-menu');
+            document.all.subcontent03.style.visibility="hidden";
+            ($('.content-sub-menu-04')).removeClass('selected-sub-menu');
+            document.all.subcontent04.style.visibility="hidden";
+            ($('.content-sub-menu-05')).addClass('selected-sub-menu');
+            document.all.subcontent05.style.visibility="visible";
+            break;
+
+        default:
+            alert("잘못된 값이 입력되었습니다.");
+            break;
+    }
+}
