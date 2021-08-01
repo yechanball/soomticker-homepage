20f50820a2baec353dd06e43b0a3a6947423dce9
yechan-nfc <77843815+yechan-nfc@users.noreply.github.com>
2021-08-01 22:54:33 +0900

    Add Google Analytics Global site tag (gtag.js)

---
+++ js/gtag.js
@@ -0,0 +1,5 @@
+// Global site tag (gtag.js) - Google Analytics 
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-PKJ3FC185S');
